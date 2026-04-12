e85245a0dfc43bad2b2d085ba4e464dc70fa7741
oojjrs <oojjrs@gmail.com>
2026-04-12 11:09:35 +0900

    세션 예외 오류 문자열 추가와 패키지 버전 갱신

--- Assets/MyNetSessionException.cs
+++ Assets/MyNetSessionException.cs
@@ -6,6 +6,7 @@ namespace oojjrs.onet
     public class MyNetSessionException : Exception
     {
         public SessionError Error { get; private set; }
+        public string ErrorString => Error.ToString();
 
         internal MyNetSessionException(string message, SessionError error, Exception innerException)
             : base(message, innerException)
